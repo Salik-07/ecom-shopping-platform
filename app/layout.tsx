038a86ac00317d3022657bb370f8c51a1c2d036c
Salik-07 <salikhussain41@gmail.com>
2025-01-04 13:27:08 +0500

    Add root laytout and constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ecom Shopping",
-  description: "An ecommerce platform",
+  title: {
+    template: `%s | Ecom Shopping`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
