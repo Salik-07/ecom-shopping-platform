8d79f52c1b7f2b8270c5f4cd60d39fa0fd9837f8
Salik-07 <salikhussain41@gmail.com>
2025-01-05 19:58:18 +0500

    Add product list, product price and product card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,34 @@
-import { Button } from "@/components/ui/button";
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+// import {
+//   getLatestProducts,
+//   getFeaturedProducts,
+// } from "@/lib/actions/product.actions";
+// import ProductCarousel from "@/components/shared/product/product-carousel";
+// import ViewAllProductsButton from "@/components/view-all-products-button";
+// import IconBoxes from "@/components/icon-boxes";
+// import DealCountdown from "@/components/deal-countdown";
 
-const HomePage = async () => {
-  return <Button>Click!</Button>;
+const Homepage = async () => {
+  // const latestProducts = await getLatestProducts();
+  // const featuredProducts = await getFeaturedProducts();
+
+  return (
+    <>
+      {/* {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )} */}
+      <ProductList
+        // data={latestProducts}
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+      {/* <ViewAllProductsButton />
+      <DealCountdown />
+      <IconBoxes /> */}
+    </>
+  );
 };
 
-export default HomePage;
+export default Homepage;
